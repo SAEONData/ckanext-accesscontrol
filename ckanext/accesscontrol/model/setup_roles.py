d3b63792dc9ba9d491a30b57fec7e894f0070bc8
Mark Jacobson <35484465+mark-saeon@users.noreply.github.com>
2019-03-01 09:56:37 +0200

    Reorder table creation for foreign key refs

--- ckanext/accesscontrol/model/setup_roles.py
+++ ckanext/accesscontrol/model/setup_roles.py
@@ -11,12 +11,12 @@ def init_tables():
     tables = (
         role_table,
         role_revision_table,
+        permission_table,
+        permission_action_table,
         role_permission_table,
         role_permission_revision_table,
         user_role_table,
         user_role_revision_table,
-        permission_table,
-        permission_action_table,
     )
     for table in tables:
         if not table.exists():
